0c708ae83634a8723dc864f3a9c88cab32dc45eb
swarajkamila437-create <swarajkamila437@gmail.com>
2026-01-05 22:35:47 +0530

    Add files via upload

---
+++ module4task2.py
@@ -0,0 +1,8 @@
+import math
+num = float(input("Enter a number: "))
+sqrt = math.sqrt(num)
+log = math.log(num)
+sin = math.sin(num)
+print("Square root of the number:",sqrt)
+print("Natural logarithm of the number:",log)
+print("Sine of the number:",sin)
